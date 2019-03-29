a9644a8838af4a25bdf3eaecfb798ac5d5a43358
Matthew Tondreau <mmtondreau@gmail.com>
2019-03-28 21:54:14 -0700

    fix logger

--- dist/logger.js
+++ dist/logger.js
@@ -1,15 +1,13 @@
-var logger = (function Logger() {
-   var frame = [];
-   return { 
-      entry : function(name) {
-         frame.push(name);
-      },
-      exit : function(name) {
-         frame.pop();
-      },
-      debug: function(stmt) {
-         console.log("[DEBUG] " + frame[frame.length-1] + " - " + stmt);
-      }
+var frame = [];
+var logger = { 
+   entry : function(name) {
+      frame.push(name);
+   },
+   exit : function(name) {
+      frame.pop();
+   },
+   debug: function(stmt) {
+      console.log("[DEBUG] " + frame[frame.length-1] + " - " + stmt);
    }
-})();
+};
 module.export = logger;
