c39c4db8073f0ac2d73083c846e3388ec6b8ecf7
Matthew Tondreau <mmtondreau@gmail.com>
2019-03-29 19:55:52 -0700

    Add harvester random logic

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -1,28 +1,43 @@
+
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
+   /** @param {Creep} creep **/
+   run: function(creep) {
+      if(creep.carry.energy < creep.carryCapacity) {
+         if (creep.memory.target != null) {
+            creep.memory.target = null;
+         }
+         if (creep.memory.source == null) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
+            creep.memory.source = sources(Math.floor(Math.random()*sources)];
+         }
+         if(creep.harvest(creep.memory.source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}});
+         }
+      }
+      else {                         
+         if (creep.memory.source != null) {
+            creep.memory.source = null;
+         }
+         if (creep.memory.target == null) {
             var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
+               filter: (structure) => {
+                  return (structure.structureType == STRUCTURE_EXTENSION ||
+                     structure.structureType == STRUCTURE_SPAWN ||
+                     structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+               }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+               creep.memory.target = targets[Math.floor(Math.random() * targets.length)];
             }
-        }
-    }
+         } else {
+            if(creep.transfer(creep.memory.target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(creep.memory.target, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+         }
+
+      }
+   }
 };
 
 module.exports = roleHarvester;
