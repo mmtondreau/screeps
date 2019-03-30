dcb78e4d021a5bf84cf68f2ee3fa45c586230d01
Matthew Tondreau <mmtondreau@gmail.com>
2019-03-29 20:50:55 -0700

    add logger

--- dist/logger.js
+++ dist/logger.js
@@ -1,50 +1,49 @@
 
-var frame = [];
-
-LOG_LEVELS = {
-   TRACE: 0,
-   DEBUG: 1,
-   INFO: 2,
-   WARN: 3,
-   ERROR: 4
-}
-                        
-var logger = {
-   logLevel: function() {
-      return LOG_LEVELS[Memory.logLevel] || 1;
-   },
-   entry : function(name) {
-      frame.push(name);
-      logger.trace("entry");
-
-   },
-   exit : function(name) {
-      logger.trace("exit");
-      frame.pop();
-   },
-   debug: function(stmt) {
-      if (logger.logLevel() <= LOG_LEVELS.DEBUG) {
-         console.log("[DEBUG] " + frame[frame.length-1] + " - " + stmt);
-      }
-   },
-   trace: function(stmt) {
-      if (logger.logLevel() <= LOG_LEVELS.TRACE) {
-         console.log("[TRACE] " + frame[frame.length-1] + " - " + stmt);
-      }
-   },
-   info: function(stmt) {
-      if (logger.logLevel() <= LOG_LEVELS.INFO) {
-         console.log("[INFO] " + frame[frame.length-1] + " - " + stmt);
-      }
-   },
-   warn: function(stmt) {
-      if (logger.logLevel() <= LOG_LEVELS.WARN) {
-         console.log("[WARN] " + frame[frame.length-1] + " - " + stmt);
-      }
-   },
-   error: function(stmt) {
-      console.log("[ERROR] " + frame[frame.length-1] + " - " + stmt);
+function Logger() {
+   this.LOG_LEVELS = {
+      TRACE: 0,
+      DEBUG: 1,
+      INFO: 2,
+      WARN: 3,
+      ERROR: 4
    }
-};
+   this.frame = [];
 
-module.exports = logger;
+};                        
+Logger.prototype.logLevel = function() {
+   return this.LOG_LEVELS[Memory.logLevel] || 1;
+}
+Logger.prototype.entry = function(name) {
+   this.frame.push(name);
+   this.trace("entry");
+
+}
+Logger.prototype.exit = function(name) {
+   this.trace("exit");
+   this.frame.pop();
+}
+Logger.prototype.debug = function(stmt) {
+   if (this.logLevel() <= this.LOG_LEVELS.DEBUG) {
+      console.log("[DEBUG] " + this.frame[this.frame.length-1] + " - " + stmt);
+   }
+}
+Logger.prototype.trace = function(stmt) {
+   if (this.logLevel() <= this.LOG_LEVELS.TRACE) {
+      console.log("[TRACE] " + this.frame[this.frame.length-1] + " - " + stmt);
+   }
+}
+Logger.prototype.info: function(stmt) {
+   if (this.logLevel() <= this.LOG_LEVELS.INFO) {
+      console.log("[INFO] " + this.frame[this.frame.length-1] + " - " + stmt);
+   }
+}
+Logger.prototype.warn = function(stmt) {
+   if (this.logLevel() <= this.LOG_LEVELS.WARN) {
+      console.log("[WARN] " + this.frame[this.frame.length-1] + " - " + stmt);
+   }
+}
+Logger.prototype.error = function(stmt) {
+   console.log("[ERROR] " + this.frame[this.frame.length-1] + " - " + stmt);
+}
+
+module.exports = new Logger();;
